a4ded79bf6724c0d56ce3c005c1c4db69dec3286
Ayush <iamayush07052061@gmail.com>
2024-12-30 16:29:00 +0545

    login page css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from'react-router-dom';
-import Home from './pages/Home';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
+import Home from './pages/Home';
 function App() {
   return (
     <div className="App">
        <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Home/>} />
       <Route path="/signup" element={<Signup/>}/>
       <Route path="/login" element={<Login/>}/>
+      <Route path="/" element={<Home/>} />
     </Routes>
   </BrowserRouter>
     </div>
