2bc86b35cf2ec10bc472f423b3ebc8b68f8d440a
Sadikshyaaa143Q <105156284+Sadikshyaaa143Q@users.noreply.github.com>
2025-01-23 18:39:24 +0545

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import{ useNavigate, Link } from 'react-router-dom';
+import{ Link, useNavigate } from 'react-router-dom';
 import Header from "./Header";
 import axios from "axios";
 import Categories from "./Categories";
@@ -13,11 +13,12 @@ import { IoIosAddCircleOutline } from "react-icons/io";
 
 
 function Home(){
-    //const navigate= useNavigate()
+    const navigate= useNavigate()
     const[products, setproducts] =useState([]);
     const[page,setPage]=useState(1);
-    const [totalPages, setTotalpages]=useState(0); //we don't the the total number of pages that we will have
+ //   const [totalPages, setTotalpages]=useState(0); //we don't the the total number of pages that we will have
     const[search, setsearch] =useState([]); 
+    const[issearch, setissearch] =useState(false);
     const[cproducts, setcproducts] =useState([]); //the list of the products is over-ridden every time so euta category ko products only are stored
     // useEffect(()=>{
     //     if(!localStorage.getItem('token')){
@@ -54,19 +55,31 @@ function Home(){
         setsearch(value);
     }
     const handleClick=()=>{
+        const url='http://localhost:8000/search?search=' + search;
+        axios.get(url)
+        .then((res)=>{
+            //console.log(res.data) ;
+            setcproducts(res.data.products);
+            setissearch(true);
+         })
+         .catch((_err)=>{
+            //console.log(_err)
+             alert('SERVER ERR')
+         })
        // console.log('clicked');
        //console.log('products',products); //here we have the list of products now can find the one
-        let filteredProducts=products.filter((item)=>{
-            if(item.pname.toLowerCase().includes(String(search).toLowerCase())|| item.pdesc.toLowerCase().includes(String(search).toLowerCase())|| item.category.toLowerCase().includes(String(search).toLowerCase())){
-                return item; //after the first step filtered products=item and setproducts(item ) works making the home look like a filtered search
-            }
-        })
-       setcproducts(filteredProducts)
+        // let filteredProducts=products.filter((item)=>{
+        //     if(item.pname.toLowerCase().includes(String(search).toLowerCase())|| item.pdesc.toLowerCase().includes(String(search).toLowerCase())|| item.category.toLowerCase().includes(String(search).toLowerCase())){
+        //         return item; //after the first step filtered products=item and setproducts(item ) works making the home look like a filtered search
+        //     }
+        // })
+
+       //setcproducts(filteredProducts)
     }
     const handleCategory=(value)=>{
         //console.log(value)
         let filteredProducts=products.filter((item)=>{
-            if( item.category.toLowerCase() ==(value.toLowerCase())){
+            if( item.category.toLowerCase() ===(value.toLowerCase())){
                 return item; //after the first step filtered products=item and setproducts(item ) works making the home look like a filtered search
             }
         })
@@ -91,48 +104,48 @@ function Home(){
              alert('SERVER ERR')
          })
        }
+
+    const handleProduct = (id) =>{
+       navigate('/product/'+ id )
+       }
+       const setClear=()=>{
+        setissearch(false)
+        setsearch('')
+       }
     return (
         <div> 
             <Header search={search} handlesearch={handlesearch} handleClick={handleClick} />
-            <h1> WELCOME to home...</h1>
             <Categories handleCategory={handleCategory} />
             {localStorage.getItem('token') &&<Link  to="/add-product"><button className="logout-btn mt-3 mb-3"> Add Product<IoIosAddCircleOutline className="icons"/></button> </Link>}
-            {/* <h2>
-                MY PRODUCTS:
-            </h2> */}
-            <h5>Search Result</h5>
-            <div className="d-flex justify-content-center flex-wrap">
-            {cproducts && products.length>0 &&
-            cproducts.map((item,index)=>{
-                return( 
-                    <div key={item._id} className="card  m-3 "> 
-                    <div onClick={()=>handleLike(item._id)} className="icon-con"> 
-                        <CiHeart className="icons"/> 
-                    </div>
-                    
-                {/* siuf whatever we type here will be shown on the homepage and we want to show the products on the homepage, if written within curlybraces then it is infact dynamic in nature*/}
-                    <img width="300px" height ="300px" src={'http://localhost:8000/'+ item.pimage} alt={item.pname}/>
-                    <p className="m-2"> {item.pname} | {item.category}</p> 
-                    <h3 className="m-2 text-danger" > Rs. {item.price}</h3>
-                    <p className="m-2 text-success" > {item.pdesc}</p> 
-                     
-                    
-                    
-                    </div>
-
-
-
-                )
-            })
-            }
+            { issearch && cproducts && <h5> Search Results
+               <button className="clear-btn" onClick={()=>setClear()}>Clear</button> 
+                </h5>}
+            { issearch && cproducts && cproducts.length==0 && <h5>No result found</h5>}
+        { issearch &&  <div className="d-flex justify-content-center flex-wrap">
+        {cproducts && products.length>0 &&
+        cproducts.map((item,index)=>{
+            return( 
+                <div key={item._id} className="card  m-3 "> 
+                <div onClick={()=>handleLike(item._id)} className="icon-con"> 
+                    <CiHeart className="icons"/> 
+                </div>
+                
             
-         </div>
-         <h5> ALL RESULTS</h5>
-            <div className="d-flex justify-content-center flex-wrap">
+                <img width="300px" height ="300px" src={'http://localhost:8000/'+ item.pimage} alt={item.pname}/>
+                <p className="m-2"> {item.pname} | {item.category}</p> 
+                <h3 className="m-2 text-danger" > Rs.{item.price}/-</h3>
+                <p className="m-2 text-success" > {item.pdesc}</p> 
+                </div>
+            )
+        })
+        }
+        
+        </div>}
+          { !issearch &&  <div className="d-flex justify-content-center flex-wrap">
             {products && products.length>0 &&
             products.slice(page*1-1,page*1).map((item,index)=>{
                 return( 
-                    <div key={item._id} className="card  m-3 "> 
+                    <div onClick={()=> handleProduct(item._id)} key={item._id} className="card  m-3 "> 
                     <div onClick={()=>handleLike(item._id)} className="icon-con">
                         {/* this gives the product id */}
                         <CiHeart className="icons"/> 
@@ -141,7 +154,7 @@ function Home(){
                 {/* siuf whatever we type here will be shown on the homepage and we want to show the products on the homepage, if written within curlybraces then it is infact dynamic in nature*/}
                     <img width="300px" height ="300px"  alt={item.pname}src={'http://localhost:8000/'+ item.pimage}/>
                     <p className="m-2"> {item.pname} | {item.category}</p> 
-                    <h3 className="m-2 text-danger" > Rs. {item.price}</h3>
+                    <h3 className="m-2 price" >Rs.{item.price}/-</h3>
                     <p className="m-2 text-success" > {item.pdesc}</p> 
                      
                     
@@ -153,9 +166,9 @@ function Home(){
                 )
             })
             }
-         </div>
+         </div>}
          {
-            products.length>0 && <div className="pagination">
+           !issearch && products.length>0 && <div className="pagination">
                 <span onClick={()=>selectPageHandler(page-1) } className={page > 1 ? "" : "pagination__disable"}>👈</span>
                 {
                     [...Array(Math.ceil(products.length/1))].map((_,i)=>{
