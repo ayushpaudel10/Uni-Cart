161042c7738fc74add4c82184f772e30ebfcd3c5
Sadikshyaaa143Q <105156284+Sadikshyaaa143Q@users.noreply.github.com>
2025-02-11 10:09:03 +0545

    Update AddProduct.jsx

--- src/components/AddProduct.jsx
+++ src/components/AddProduct.jsx
@@ -1,95 +1,4 @@
-// import { useEffect } from "react";
-// import{ useNavigate, Link } from 'react-router-dom';
-// import Header from "./Header";
-// import { useState } from "react";
-// import axios from "axios";
 
-
-
-// function AddProduct(){
-//     const navigate= useNavigate();
-//     const[pname,setpname]=useState('');
-//     const[pdesc,setpdesc]=useState('');
-//     const[price,setprice]=useState('');
-//     const[category,setcategory]=useState('');
-//     const[pimage,setpimage]=useState('');
-//     const url='http://localhost:8000/add-product';
-
-//     useEffect(()=>{
-//         if(!localStorage.getItem('token')){
-//             navigate('/login') //if token is not there then 1st login then come to homepage
-//         }
-//     },[])
-//     const handleApi=()=>{
-//         const formData= new FormData();
-//         formData.append('pname',pname)
-//         formData.append('pdesc',pdesc)
-//         formData.append('price',price)
-//         formData.append('category',category)
-//         formData.append('pimage',pimage)  
-//         axios.post(url,formData)
-//         .then((res) => {
-//             console.log(res)
-//             if(res.data.message){
-//                 alert(res.data.message);
-//                 navigate('/')
-//             }
-//         })
-//         .catch((err)=>{
-//             console.log(err)
-//         })
-
-//     }
-//     return (
-//         <div>
-//             <Header />
-//             <div className="p-3"> 
-//             <h2> ADD PRODUCT HERE </h2>
-//             <label>Product Name</label>
-//             <input className="form-control" type="text" value={pname}
-//             onChange={(e)=>{
-//                 setpname(e.target.value)
-//             }} />
-//             <label>Product Description</label>
-//             <input className="form-control" type="text" value={pdesc}
-//               onChange={(e)=>{
-//                 setpdesc(e.target.value)
-//             }}
-//              />
-//             <label>Product Price</label>
-//             <input className="form-control" type="text" value={price}
-//              onChange={(e)=>{
-//                 setprice(e.target.value)
-//             }}
-//             />
-//             <label>Product Category</label>
-//             <select className="form-control" value={category}
-//              onChange={(e)=>{
-//                 setcategory(e.target.value)
-//             }}>
-//                 <option>Bikes</option>
-//                 <option>Mobile</option>
-//                 <option>Cloth</option>
-//                 </select>
-//                 <label>Product Image</label>
-//             <input className="form-control" type="file"
-//              onChange={(e)=>{
-//                 setpimage(e.target.files[0])
-//             }}
-//             />
-//         <button onClick ={handleApi} className="btn btn-primary mt-3"> SUBMIT</button>
-                 
-
-            
-//             </div>
-           
-
-
-//         </div>
-//     )
-// }
-// export default AddProduct;//this when only one file to be imported if multiples then something 
-// else
 import { useEffect } from "react";
 import{ useNavigate, Link } from 'react-router-dom';
 import Header from "./Header";
@@ -99,13 +8,14 @@ import './AddProduct.css'
 import Categories from "./CategoriesList";
 function AddProduct(){
     const navigate= useNavigate();
-    const[pname,setpname]=useState(' ');
-    const[pdesc,setpdesc]=useState(' ');
-    const[price,setprice]=useState(' ');
+    const[pname,setpname]=useState('');
+    const[pdesc,setpdesc]=useState('');
+    const[price,setprice]=useState('');
     const [mainCategory, setMainCategory] = useState("");
     const [subCategory, setSubCategory] = useState("");
     const [swap, setswap] = useState(false);
-    const[pimage,setpimage]=useState(' ');
+    const [message, setMessage]=useState("");
+    const[pimage,setpimage]=useState('');
     const url='http://localhost:8000/add-product';
     useEffect(()=>{
         if(!localStorage.getItem('token')){
@@ -141,6 +51,13 @@ function AddProduct(){
             console.log(err)
         })
     }
+    const handleBlur = () => {
+      if (!/^\d+(\.\d+)?$/.test(price)) {
+        setMessage("Price must be numeric value.");
+      } else {
+        setMessage("");
+      }
+    };
     return <>
         <div>
             <Header />
@@ -149,7 +66,7 @@ function AddProduct(){
             <h2 class="add-items-heading">ADD ITEMS</h2>
             <div class="line"></div>
             <br/>
-
+            {message && <p className="message">{message}</p>}
             <div className="form-group">
             <label>Name of the product</label>
             <input className="form-control" type="text" value={pname}
@@ -161,6 +78,7 @@ function AddProduct(){
            <div className="form-group-2">
             <label>Price</label>
             <input className="form-control" type="text" inputMode="numeric" value={price}
+            onBlur={handleBlur}
              onChange={(e)=>{
                 setprice(e.target.value)
             }}
@@ -228,7 +146,7 @@ function AddProduct(){
             }}
             />
         
-        <button onClick ={handleApi} className="btn btn-primary mt-3">ADD</button>
+        <button onClick ={handleApi} disabled={message} className="btn btn-primary mt-3">ADD</button>
         </div>   
         </div>     
             
